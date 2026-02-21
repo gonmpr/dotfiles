a1e0dde7192a5677887179d1b248cfab4378f07c
gonmpr <gmporrorosas@gmail.com>
2026-02-20 21:18:38 -0300

    adding theme

--- .config/scripts/ch_theme.sh
+++ .config/scripts/ch_theme.sh
@@ -17,8 +17,16 @@ echo "@import \"../themes/$THEME/waybar/style.css\";" > ~/.config/waybar/style.c
 echo "@import \"../themes/$THEME/swaync/style.css\";" > ~/.config/swaync/style.css
 echo "@import \"$BASE_DIR/$THEME/rofi/launcher.rasi\"" > ~/.config/rofi/config.rasi
 
+# change gtk theme (TODO)
+if [[ "$THEME" == *"morning"* ]] || [[ "$THEME" == *"white"* ]]; then
+    gsettings set org.gnome.desktop.interface gtk-theme "Granite" 2>/dev/null
+    gsettings set org.gnome.desktop.interface color-scheme 'prefer-light' 2>/dev/null
+else
+    gsettings set org.gnome.desktop.interface gtk-theme "adw-gtk3-dark" 2>/dev/null
+    gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark' 2>/dev/null
+fi
 
+# kill and restart apps
 killall waybar 2>/dev/null && waybar &
-killall swaync 
+killall swaync 2>/dev/null
 swaync &
-
