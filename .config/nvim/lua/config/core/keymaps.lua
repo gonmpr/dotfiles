f090f9398e44531c70fb9b85d46423f528529e27
Gonzalo Martin Porro Rosas <121343797+gonmpr@users.noreply.github.com>
2026-06-18 22:33:08 -0300

    Update keymaps.lua

--- .config/nvim/lua/config/core/keymaps.lua
+++ .config/nvim/lua/config/core/keymaps.lua
@@ -9,15 +9,6 @@ vim.keymap.set("i", "jj", "<Esc>", { desc = "Esc" })
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Half page down (centered)" })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Half page up (centered)" })
 
-
--- Automatically close quotes, brackets, etc.
-vim.keymap.set('i', "'", "''<left>", { noremap = true, silent = true })
-vim.keymap.set('i', '"', '""<left>', { noremap = true, silent = true })
-vim.keymap.set('i', "(", "()<left>", { noremap = true, silent = true })
-vim.keymap.set('i', "[", "[]<left>", { noremap = true, silent = true })
-vim.keymap.set('i', "{", "{}<left>", { noremap = true, silent = true })
-
-
 -- Replace all instances of highlighted words
 vim.keymap.set("v", "r", "\"hy:%s/<C-r>h//g<left><left>")			
 
@@ -26,11 +17,9 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv")
 -- Move current line up 
 vim.keymap.set("v", "K", ":m '<-2<CR>gv")
 
-
 -- Better indenting in visual mode
 vim.keymap.set("v", "<", "<gv", { desc = "Indent left and reselect" })
 vim.keymap.set("v", ">", ">gv", { desc = "Indent right and reselect" })
-
 
 -- Buffer navigation
 vim.keymap.set("n", "<Tab>", ":bnext<CR>", { desc = "Next buffer" })
