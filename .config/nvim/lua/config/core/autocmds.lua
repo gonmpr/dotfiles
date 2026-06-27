817b53785e6656f4b9db29d75c5c41a3757fd0ae
Gonzalo Martin Porro Rosas <121343797+gonmpr@users.noreply.github.com>
2026-06-27 20:11:01 -0300

    Update autocmds.lua

--- .config/nvim/lua/config/core/autocmds.lua
+++ .config/nvim/lua/config/core/autocmds.lua
@@ -6,18 +6,21 @@ local augroup = vim.api.nvim_create_augroup("core_autocmds", {
 vim.api.nvim_create_autocmd("BufReadPost", {
   group = augroup,
   callback = function()
-    local mark = vim.api.nvim_buf_get_mark(0, '"')
-    local lcount = vim.api.nvim_buf_line_count(0)
-    local line = mark[1]
-    local ft = vim.bo.filetype
-    if line > 0 and line <= lcount
-      and vim.fn.index({ "commit", "gitrebase", "xxd" }, ft) == -1
-      and not vim.o.diff then
-      pcall(vim.api.nvim_win_set_cursor, 0, mark)
-    end
+    vim.schedule(function()
+      local mark = vim.api.nvim_buf_get_mark(0, '"')
+      local lcount = vim.api.nvim_buf_line_count(0)
+      local line = mark[1]
+      local ft = vim.bo.filetype
+
+      if line > 0
+        and line <= lcount
+        and vim.fn.index({ "commit", "gitrebase", "xxd" }, ft) == -1
+        and not vim.o.diff then
+        pcall(vim.api.nvim_win_set_cursor, 0, mark)
+      end
+    end)
   end,
 })
-
 
 -- No auto continue comments on new line
 vim.api.nvim_create_autocmd("FileType", {
@@ -37,4 +40,3 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank({ timeout = 200, visual = true })
 	end,
 })
-
